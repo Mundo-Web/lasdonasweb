da70ac3ac2037da04f075154412e4e813fc479f7
Carlos Colina <carlosecolina89@gmail.com>
2024-08-25 18:10:02 -0500

    actualizacion index

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -33,8 +33,8 @@
                 <th>Stock</th>
                 {{-- <th>Peso</th> --}}
                 <th>Imagen</th>
-                <th>Lo más pedido</th>
-                <th>Novedad</th>
+
+                <th>Novedades</th>
                 {{--  <th>Liquidacion</th> --}}
                 <th>Visible</th>
                 <th>Acciones</th>
@@ -61,7 +61,7 @@
 
 
                   </td>
-                  <td>
+                  {{--  <td>
                     <form method="POST" action="">
                       @csrf
                       <input type="checkbox" id="hs-basic-usage"
@@ -75,7 +75,7 @@
                         data-titleService='{{ $item->producto }}' {{ $item->destacar == 1 ? 'checked' : '' }}>
                       <label for="{{ 'v_' . $item->id }}"></label>
                     </form>
-                  </td>
+                  </td> --}}
                   <td>
                     <form method="POST" action="">
                       @csrf
