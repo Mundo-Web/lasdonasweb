305f227c16260baebee2571c6294ad72c5a8017b
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-25 19:37:59 -0500

    Abajo aparece la miniatura

--- resources/views/components/product/card.blade.php
+++ resources/views/components/product/card.blade.php
@@ -10,34 +10,36 @@
             @if ($image->name_imagen)
               <img src="{{ asset($image->name_imagen) }}" alt="{{ $image->name_imagen }}"
                 class="w-full aspect-square object-cover hover:scale-110 transition-transform duration-300" />
-            @else
-              <img src="{{ asset('images/img/noimagen.jpg') }}" alt="{{ $item->producto }}"
-                class="w-full aspect-square object-cover hover:scale-110 transition-transform duration-300" />
-            @endif
+            @break
+          @else
+            <img src="{{ asset('images/img/noimagen.jpg') }}" alt="{{ $item->producto }}"
+              class="w-full aspect-square object-cover hover:scale-110 transition-transform duration-300" />
           @endif
-        @endforeach
+        @endif
+      @endforeach
+    @endif
+  </div>
+  <div class="p-4">
+    <h2 class="block text-xl text-[#112212] mb-1 font-bold truncate">{{ $item->producto }}</h2>
+    <p
+      class="text-base font-normal text-[rgba(17,34,18,0.8)] line-clamp-2 text-ellipsis h-[48px] mb-1 font-b_classic_regular tracking-wide">
+      {{ $item->extract }}
+    </p>
+    <div class="flex items-center  space-x-2">
+      @if ($item->descuento > 0)
+        <p class="text-[#112212] font-bold">S/ <span>{{ $item->descuento }}</span></p>
+        <p class="text-[rgba(17,34,18,0.8)] line-through text-sm">S/ <span>{{ $item->precio }}</span></p>
+      @else
+        <p class="text-[#112212] font-bold">S/ <span>{{ $item->precio }}</span></p>
       @endif
     </div>
-    <div class="p-4">
-      <h2 class="block text-xl text-[#112212] mb-1 font-bold truncate">{{ $item->producto }}</h2>
-      <p class="text-base font-normal text-[rgba(17,34,18,0.8)] line-clamp-2 text-ellipsis h-[48px] mb-1 font-b_classic_regular tracking-wide">
-        {{ $item->extract }}
-      </p>
-      <div class="flex items-center  space-x-2">
-        @if ($item->descuento > 0)
-          <p class="text-[#112212] font-bold">S/ <span>{{ $item->descuento }}</span></p>
-          <p class="text-[rgba(17,34,18,0.8)] line-through text-sm">S/ <span>{{ $item->precio }}</span></p>
-        @else
-          <p class="text-[#112212] font-bold">S/ <span>{{ $item->precio }}</span></p>
-        @endif
-      </div>
-    </div>
-  </a>
-  <div class="w-full mt-4">
-    <a href="{{ route('Product.jsx', $item->id) }}" type="button"
-      class="w-full py-3 rounded-full shadow-md font-medium flex items-center justify-center bg-[#336234] text-white text-base hover:bg-[#2d5228] transition-colors duration-300">
-      <span>Ver producto</span>
-      <i class="ml-2 fa-solid fa-arrow-right"></i>
-    </a>
   </div>
+</a>
+<div class="w-full mt-4">
+  <a href="{{ route('Product.jsx', $item->id) }}" type="button"
+    class="w-full py-3 rounded-full shadow-md font-medium flex items-center justify-center bg-[#336234] text-white text-base hover:bg-[#2d5228] transition-colors duration-300">
+    <span>Ver producto</span>
+    <i class="ml-2 fa-solid fa-arrow-right"></i>
+  </a>
+</div>
 </div>
