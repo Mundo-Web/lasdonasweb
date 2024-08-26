c1a95843e2d9a4afade59ff93d1c1c1d40f20e3e
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-08-26 17:07:03 -0500

    final

--- resources/views/components/category/container.blade.php
+++ resources/views/components/category/container.blade.php
@@ -2,7 +2,7 @@
   class="flex flex-col relative" data-aos="zoom-in-left">
   <div class="bg-colorBackgroundProducts rounded-sm product_container basis-4/5 flex flex-col justify-center relative">
     <div>
-      <div class="relative flex justify-center items-center w-full h-[500px] bg-cover object-cover">
+      <div class="relative flex justify-center items-center w-full h-[300px] bg-cover object-cover aspect-square">
         @if ($item->url_image)
           <img x-transition:enter="transition ease-out duration-300 transform"
             x-transition:enter-start="opacity-100 scale-100" x-transition:enter-end="opacity-100 scale-105"
@@ -10,7 +10,7 @@
             x-transition:leave-start="opacity-100 scale-105" x-transition:leave-end="opacity-100 scale-100"
             :class="{ 'scale-105': showAmbiente, 'scale-100': !showAmbiente }"
             src="{{ asset($item->url_image) }}/{{ $item->name_image }}" alt="{{ $item->name }}"
-            class="w-full h-full object-cover absolute inset-0 transition-transform duration-300" />
+            class="w-full h-full object-cover absolute inset-0 transition-transform duration-300 aspect-square" />
         @else
           <img x-transition:enter="transition ease-out duration-300 transform"
             x-transition:enter-start="opacity-100 scale-100" x-transition:enter-end="opacity-100 scale-105"
