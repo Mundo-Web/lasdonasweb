e3ed945150978456e2802d4e816c1cb06d3dfd47
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-26 12:45:25 -0500

    NTS

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use SoDe\Extend\Text;
 
 class CategorySeeder extends Seeder
 {
@@ -27,6 +28,7 @@ class CategorySeeder extends Seeder
             Category::updateOrCreate([
                 'name' => $category
             ], [
+                'slug' => Text::slug($category),
                 'description' => 'Descripcion de ' . $category,
                 'status' => true,
                 'visible' => true,
