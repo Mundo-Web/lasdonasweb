21b37fd713ecf3f103ad5b676c940fbf1a0ed6cd
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-28 17:46:10 -0500

    NTS

--- resources/js/components/ReceiptTypeSelector.jsx
+++ resources/js/components/ReceiptTypeSelector.jsx
@@ -1,13 +1,14 @@
 function ReceiptTypeSelector({onChange}) {
   return (
-    <div className="flex flex-wrap gap-8 items-center mt-8 w-full text-sm font-bold tracking-wide whitespace-nowrap max-md:max-w-full">
-      <label className="flex gap-1 items-center self-stretch my-auto text-neutral-900 text-opacity-60">
+    <div className="flex flex-wrap gap-8 items-center mt-2 w-full text-sm font-bold tracking-wide whitespace-nowrap max-md:max-w-full">
+      <label className="flex gap-1 items-center self-stretch my-auto text-neutral-900">
         <input
           type="radio"
           name="receiptType"
           value="boleta"
           className="hover:text-orange-400 hover:border-orange-400 focus:ring-0 focus:ring-offset-0 focus:outline-none checked:bg-orange-400 checked:border-orange-400"
           onChange={() => onChange('boleta')}
+          required
         />
         <span className="self-stretch my-auto">Boleta</span>
       </label>
@@ -18,6 +19,7 @@ function ReceiptTypeSelector({onChange}) {
           value="factura"
           className="hover:text-orange-400 hover:border-orange-400 focus:ring-0 focus:ring-offset-0 focus:outline-none checked:bg-orange-400 checked:border-orange-400"
           onChange={() => onChange('factura')}
+          required
         />
         <span className="self-stretch my-auto">Factura</span>
       </label>
