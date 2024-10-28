29715e640b6824b7666ed4a7d6a1f9e1e3644304
Carlos Colina <carlosecolina89@gmail.com>
2024-10-28 13:19:17 -0500

    actualizacion mailing

--- app/Http/Controllers/MailingController.php
+++ app/Http/Controllers/MailingController.php
@@ -7,6 +7,7 @@ use App\Models\Horarios;
 use App\Models\Ordenes;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 use SoDe\Extend\File;
 use SoDe\Extend\JSON;
 use SoDe\Extend\Text;
@@ -16,47 +17,59 @@ class MailingController extends Controller
     static function notifyPoints(User $userJpa, Ordenes $ordenJpa, Horarios $horarioJpa, $generals)
     {
         try {
-            $content = File::get('../storage/app/utils/mailing/pagoTc.html');
+            $productos = MailingController::obtenerProductos($ordenJpa);
+
+            // $content = File::get('../storage/app/utils/mailing/pagoTc.html');
             $data =  [
-                'client' => $userJpa->toArray(),
-                'sale' => $ordenJpa->toArray(),
+                'client' => $userJpa,
+                'sale' => $ordenJpa,
                 'domain' => env('APP_DOMAIN'),
-                'horario' => $horarioJpa->toArray(),
-                'generals' => $generals->toArray()
+                'horario' => $horarioJpa,
+                'generals' => $generals,
+                'productos' => $productos
             ];
+            $content = View::make('mailing.pagoTc', $data)->render();
+
             $mail = EmailConfig::config();
-            $mail->Subject = 'Compra Procesada ' ;
+            $mail->Subject = 'Compra Procesada ';
             $mail->isHTML(true);
-            $mail->Body = Text::replaceData($content, JSON::flatten($data), [
-                'client.name' => fn($x) => explode(' ', $x)[0]
-            ]);
+            $mail->Body = $content;
+
+            // $mail->Body = Text::replaceData($content, JSON::flatten($data), [
+            //     'client.name' => fn($x) => explode(' ', $x)[0]
+            // ]);
             $mail->addAddress($userJpa->email, $userJpa->name);
             $mail->send();
         } catch (\Throwable $th) {
-            
+            dump($th);
         }
     }
     static function notifyTransfer(User $userJpa, Ordenes $ordenJpa, Horarios $horarioJpa, $generals)
     {
+
         try {
-            $content = File::get('../storage/app/utils/mailing/transfer.html');
+            $productos = MailingController::obtenerProductos($ordenJpa);
+            // $content = File::get('../storage/app/utils/mailing/transfer.html');
             $data =  [
-                'client' => $userJpa->toArray(),
-                'sale' => $ordenJpa->toArray(),
+                'client' => $userJpa,
+                'sale' => $ordenJpa,
                 'domain' => env('APP_DOMAIN'),
-                'horario' => $horarioJpa->toArray(),
-                'generals' => $generals->toArray()
+                'horario' => $horarioJpa,
+                'generals' => $generals,
+                'productos' => $productos
             ];
+            $content = View::make('mailing.transfer', $data)->render();
             $mail = EmailConfig::config();
             $mail->Subject = 'Su compra ha sido procesada, en breve validaremos su transferencia  ';
             $mail->isHTML(true);
-            $mail->Body = Text::replaceData($content, JSON::flatten($data), [
-                'client.name' => fn($x) => explode(' ', $x)[0]
-            ]);
+            $mail->Body = $content;
+            // $mail->Body = Text::replaceData($content, JSON::flatten($data), [
+            // 'client.name' => fn($x) => explode(' ', $x)[0]
+            // ]);
             $mail->addAddress($userJpa->email, $userJpa->name);
             $mail->send();
         } catch (\Throwable $th) {
-            
+            dump($th);
         }
     }
     static function ventaProces(User $userJpa, Ordenes $ordenJpa)
@@ -65,7 +78,7 @@ class MailingController extends Controller
             $content = File::get('../storage/app/utils/mailing/points.html');
             $data =  [
                 'client' => $userJpa->toArray(),
-               'sale' => $ordenJpa->toArray(),
+                'sale' => $ordenJpa->toArray(),
                 'domain' => env('APP_DOMAIN')
             ];
             $mail = EmailConfig::config();
@@ -77,7 +90,13 @@ class MailingController extends Controller
             $mail->addAddress($userJpa->email, $userJpa->name);
             $mail->send();
         } catch (\Throwable $th) {
-            
         }
+    }
+
+    static function obtenerProductos($sale)
+    {
+
+        $ordenes = Ordenes::find($sale->id);
+        return $ordenes->DetalleOrden;
     }
 }
