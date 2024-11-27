f2195893e57cefba5b1c42b23be29c430aacbc3e
sodeworld <160994235+sodeworld@users.noreply.github.com>
2024-11-27 18:18:19 -0500

    NTS

--- resources/views/pages/products/edit.blade.php
+++ resources/views/pages/products/edit.blade.php
@@ -1177,22 +1177,16 @@
     })
 
     function devolverContenido(idContenido) {
-      console.log('esta pintando este copntenido ');
-
       let html = `
 
-      <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5" id="div-form-opcion-${idContenido}">
+      <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5" id="div-form-opcion-${idContenido}">  
         <div class="md:col-span-5">
           <button type="button" class=" ml-2 bg-red-500 justify-self-center text-white px-4 h-10 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full  eliminarOpcion" style="  position: absolute;  right: 6%;">
-
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-            </svg>
-
+            <i class="fa fa-trash"></i>
           </button>
 
         </div>
-
+        
         <div class="md:col-span-5">
 
 
@@ -1261,6 +1255,33 @@
 
         </div>
 
+          <div class="md:col-span-5 mt-2">
+                  <label for="extract">Extracto Dinamico (Este se vera en el detalle del producto)</label>
+                  <div class="relative mb-2 mt-2">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <i class="fa fa-pencil text-gray-500 dark:text-gray-400"></i>
+                    </div>
+                    <input type="text" id="opcion-form-descripcion_dinamica-${idContenido}"
+                      data-name="descripcion_dinamica" name="opcion-form-descripcion_dinamica-${idContenido}"
+                      
+                      class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Extracto dinamico">
+                  </div>
+                </div>
+
+        
+
+        <div class="md:col-span-5 mt-2"
+          <div>
+            <div id="quill-opcion-${idContenido}" name="quill-opcion-${idContenido}"
+            class="h-60 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Descripción"></div>
+            <input id="quill-Inputopcion-${idContenido}" hidden="true" name="quill-opcion-${idContenido}" data-name="description" />
+
+          </div>
+         
+        </div>
+
         <div class="md:col-span-2 mt-2">
           <div class="w-full">
             <label for="sku">SKU
@@ -1269,13 +1290,9 @@
 
             <div class="relative mb-2  mt-2">
               <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" x="0" y="0" viewBox="0 0 469.336 469.336" style="enable-background:new 0 0 512 512" xml:space="preserve" class="">
-                  <g>
-                    <path d="m456.836 76.168-64-64.054c-16.125-16.139-44.177-16.17-60.365.031L45.763 301.682a10.733 10.733 0 0 0-2.688 4.587L.409 455.73a10.682 10.682 0 0 0 10.261 13.606c.979 0 1.969-.136 2.927-.407l149.333-42.703a10.714 10.714 0 0 0 4.583-2.69l289.323-286.983c8.063-8.069 12.5-18.787 12.5-30.192s-4.437-22.124-12.5-30.193zM285.989 89.737l39.264 39.264-204.996 204.997-14.712-29.434a10.671 10.671 0 0 0-9.542-5.896H78.921L285.989 89.737zm-259.788 353.4L40.095 394.5l34.742 34.742-48.636 13.895zm123.135-35.177-51.035 14.579-51.503-51.503 14.579-51.035h28.031l18.385 36.771a10.671 10.671 0 0 0 4.771 4.771l36.771 18.385v28.032zm21.334-17.543v-17.082c0-4.042-2.281-7.729-5.896-9.542l-29.434-14.712 204.996-204.996 39.264 39.264-208.93 207.068zM441.784 121.72l-47.033 46.613-93.747-93.747 46.582-47.001c8.063-8.063 22.104-8.063 30.167 0l64 64c4.031 4.031 6.25 9.385 6.25 15.083s-2.219 11.052-6.219 15.052z" fill="#9F9F9F" opacity="1" data-original="#000000" class=""></path>
-                  </g>
-                </svg>
+                <i class="fa fa-pencil"></i>
               </div>
-              <input type="text" id="opcion-form-sku-${idContenido}" data-name="sku" name="opcion-form-sku-${idContenido}" value="skudefecto" class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="# de sku">
+              <input type="text" id="opcion-form-sku-${idContenido}" data-name="sku" name="opcion-form-sku-${idContenido}" value="" class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="# de sku">
 
 
             </div>
@@ -1294,62 +1311,34 @@
           <div class="flex gap-2">
             <div class="relative mb-2  mt-2">
               <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" x="0" y="0" viewBox="0 0 469.336 469.336" style="enable-background:new 0 0 512 512" xml:space="preserve" class="">
-                  <g>
-                    <path d="m456.836 76.168-64-64.054c-16.125-16.139-44.177-16.17-60.365.031L45.763 301.682a10.733 10.733 0 0 0-2.688 4.587L.409 455.73a10.682 10.682 0 0 0 10.261 13.606c.979 0 1.969-.136 2.927-.407l149.333-42.703a10.714 10.714 0 0 0 4.583-2.69l289.323-286.983c8.063-8.069 12.5-18.787 12.5-30.192s-4.437-22.124-12.5-30.193zM285.989 89.737l39.264 39.264-204.996 204.997-14.712-29.434a10.671 10.671 0 0 0-9.542-5.896H78.921L285.989 89.737zm-259.788 353.4L40.095 394.5l34.742 34.742-48.636 13.895zm123.135-35.177-51.035 14.579-51.503-51.503 14.579-51.035h28.031l18.385 36.771a10.671 10.671 0 0 0 4.771 4.771l36.771 18.385v28.032zm21.334-17.543v-17.082c0-4.042-2.281-7.729-5.896-9.542l-29.434-14.712 204.996-204.996 39.264 39.264-208.93 207.068zM441.784 121.72l-47.033 46.613-93.747-93.747 46.582-47.001c8.063-8.063 22.104-8.063 30.167 0l64 64c4.031 4.031 6.25 9.385 6.25 15.083s-2.219 11.052-6.219 15.052z" fill="#9F9F9F" opacity="1" data-original="#000000" class=""></path>
-                  </g>
-                </svg>
+                <i class="fa fa-pencil"></i>
               </div>
-              <input type="text" id="specifications-1" name="tittleOp-1" value="" data-name="tittleOp-1" class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Titulo">
+              <input type="text" id="specifications-1" name="tittle-1" value="" data-name="tittle-1" class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Titulo">
 
             </div>
             <div class="relative mb-2  mt-2">
               <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" x="0" y="0" viewBox="0 0 469.336 469.336" style="enable-background:new 0 0 512 512" xml:space="preserve" class="">
-                  <g>
-                    <path d="m456.836 76.168-64-64.054c-16.125-16.139-44.177-16.17-60.365.031L45.763 301.682a10.733 10.733 0 0 0-2.688 4.587L.409 455.73a10.682 10.682 0 0 0 10.261 13.606c.979 0 1.969-.136 2.927-.407l149.333-42.703a10.714 10.714 0 0 0 4.583-2.69l289.323-286.983c8.063-8.069 12.5-18.787 12.5-30.192s-4.437-22.124-12.5-30.193zM285.989 89.737l39.264 39.264-204.996 204.997-14.712-29.434a10.671 10.671 0 0 0-9.542-5.896H78.921L285.989 89.737zm-259.788 353.4L40.095 394.5l34.742 34.742-48.636 13.895zm123.135-35.177-51.035 14.579-51.503-51.503 14.579-51.035h28.031l18.385 36.771a10.671 10.671 0 0 0 4.771 4.771l36.771 18.385v28.032zm21.334-17.543v-17.082c0-4.042-2.281-7.729-5.896-9.542l-29.434-14.712 204.996-204.996 39.264 39.264-208.93 207.068zM441.784 121.72l-47.033 46.613-93.747-93.747 46.582-47.001c8.063-8.063 22.104-8.063 30.167 0l64 64c4.031 4.031 6.25 9.385 6.25 15.083s-2.219 11.052-6.219 15.052z" fill="#9F9F9F" opacity="1" data-original="#000000" class=""></path>
-                  </g>
-                </svg>
+                <i class="fa fa-pencil"></i>
               </div>
-              <input type="text" id="specifications" name="specificationsOp-1" value="" data-name="specificationsOp-1" class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Valor">
-
+              <input type="text" id="specifications" name="specifications-1" value="" data-name="specifications-1" class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Valor">
             </div>
           </div>
-
-
         </div>
-
         <div class="md:col-span-5 gap-4">
           <div class="flex  flex-col gap-4 p-2">
             <label for="caratula">Caratula</label>
             <input class="hidden" type="radio" name="opcion-form-caratula" value="${idContenido}" data-name="caratula" checked>
           </div>
-
           <div class="flex flex-col gap-4">
             <input id="imagen" name="opcion-form-imagenP-${idContenido}" data-name="imagenP-${idContenido}"
               class="p-2.5 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
               aria-describedby="user_avatar_help" id="user_avatar" type="file">
             <div class="dropzone border-gray-300 dropzoneSecond " id="option-drop-${idContenido}"
-              name="opcion-attrid-${idContenido}" data-name="attrid-${idContenido}">
+              name="opcion-attrid-${idContenido}" data-name="attrid-${idContenido}" >
             </div>
-
           </div>
-
         </div>
-        <div class="md:col-span-5 mt-2">
-          <input id="quill-Inputopcion-${idContenido}" hidden="true" name="quill-opcion-${idContenido}" data-name="description" />
-
-          <div id="quill-opcion-${idContenido}" name="quill-opcion-${idContenido}"
-            class="h-60 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Descripción"></div>
-
-        </div>
-
-      </div>
-      
-      `
-
-
+      </div>`
       return html
     }
 
